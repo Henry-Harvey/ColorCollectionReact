7bc79df304cf1ccee96dbfbbb47c581ca0135594
Henry-Harvey <henryy.harveyy@gmail.com>
2021-12-04 09:39:00 -0700

    colors name

--- src/Colors.js
+++ src/Colors.js
@@ -12,43 +12,48 @@ export default function Colors({ colors }) {
   };
 
   return (
-    <div className="list-group" style={{ textAlign: "center" }}>
-      <table align="center">
-        <tr>
-          <th>Name</th>
-          <th>Hex</th>
-          <th>Timestamp</th>
-        </tr>
-        {colors?.map((color) => (
-          <tr style={{ backgroundColor: color.hex }} key={color.id}>
+    <div>
+      <div style={{ textAlign: "center" }}>
+        <h1>Colors</h1>
+      </div>
+      <div className="list-group" style={{ textAlign: "center" }}>
+        <table align="center">
+          <tr>
+            <th>Name</th>
+            <th>Hex</th>
+            <th>Timestamp</th>
+          </tr>
+          {colors?.map((color) => (
+            <tr style={{ backgroundColor: color.hex }} key={color.id}>
+              <td>
+                <button
+                  className="list-group-item"
+                  active
+                  onClick={selectColor(color)}
+                >
+                  {color.name}
+                </button>
+              </td>
+              <td>{color.hex}</td>
+              <td>{color.timestamp}</td>
+            </tr>
+          ))}
+          <tr>
             <td>
               <button
                 className="list-group-item"
                 active
-                onClick={selectColor(color)}
+                onClick={addColor}
+                style={{ textAlign: "center" }}
               >
-                {color.name}
+                Add
               </button>
             </td>
-            <td>{color.hex}</td>
-            <td>{color.timestamp}</td>
+            <td>#______</td>
+            <td>0123456789ABCDEF</td>
           </tr>
-        ))}
-        <tr>
-          <td>
-            <button
-              className="list-group-item"
-              active
-              onClick={addColor}
-              style={{ textAlign: "center" }}
-            >
-              Add
-            </button>
-          </td>
-          <td>#______</td>
-          <td>0123456789ABCDEF</td>
-        </tr>
-      </table>
+        </table>
+      </div>
     </div>
   );
 }
